ec6c9839c62506c2511380509eceae494d210d84
ValentinoTriadi <valent122016@gmail.com>
2023-10-26 17:09:18 +0700

    feat(inisialisasi): Update ascii (50%)

--- src/Spesifikasi_Program/Inisialisasi/inisialisasi.c
+++ src/Spesifikasi_Program/Inisialisasi/inisialisasi.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
  
 #define MAX_LEN 5000
-
+void delay(int number_of_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+ 
+    // Storing start time
+    clock_t start_time = clock();
+ 
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds);
+}
 void print_image(FILE *fascii)
 {
     char read_string[MAX_LEN];
     while(fgets(read_string,sizeof(read_string),fascii) != NULL)
-        printf("%s",read_string);
+    printf("%s",read_string);
     printf("\n");
+    delay(1);
 }
 void inisialisasi(){
     char *filename;
     FILE *fascii;
     system("cls || clear");
     printf("HALO\n");
-    // for (int i = 0; i < 6; i++){
-        filename = "Spesifikasi_Program/Inisialisasi/img1.txt";
+    for (int i = 0; i < 5; i++){
+        if (i == 0) {
+            filename = "Spesifikasi_Program/Inisialisasi/img5.txt";
+        } else if (i == 1){
+            filename = "Spesifikasi_Program/Inisialisasi/img4.txt";
+        } else if (i == 2){
+            filename = "Spesifikasi_Program/Inisialisasi/img3.txt";
+        } else if (i == 3){
+            filename = "Spesifikasi_Program/Inisialisasi/img2.txt";
+        } else if (i == 4){
+            filename = "Spesifikasi_Program/Inisialisasi/img1.txt";
+        }
         fascii = NULL;
         fascii = fopen(filename,"r");
         if(fascii  == NULL){
@@ -25,7 +47,6 @@ void inisialisasi(){
             system("cls || clear");
             print_image(fascii);
             fclose(fascii);
-            
         }
-    // }
+    }
 }
